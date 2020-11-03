fd4da40cd951c8de7669473aeba813b8715233da
Phillbert Tan <philly@phillytan.xyz>
2020-11-03 15:45:32 -0800

    Bug Fix

--- src/frontend/components/Devices/deleteMutation.ts
+++ src/frontend/components/Devices/deleteMutation.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export default gql`
   mutation ($_id: ID!) {
-    keys: delete_key(_id: $deviceName) {
+    keys: delete_key(_id: $_id) {
         _id
     }
   }
